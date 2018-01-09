1628315d730c6f3a41d6fccd5d8f442857daf89f
code4GG <cavallaro.av@gmail.com>
2018-01-08 23:54:12 -0500

    email/tele/password validating form

---
+++ assets/javascript/login.js
@@ -0,0 +1,40 @@
+
+	let invalid = 0;
+	
+	$('#validationForm').submit(function(event){
+		
+		event.preventDefault();
+
+		function isValidEmailAddress(emailAddress) {
+   	    const pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    	return pattern.test(emailAddress);
+		}
+		console.log("testing");
+
+		if (!isValidEmailAddress($("#email").val())){
+			invalid++
+			$('#errorEmail').show();
+
+		}
+
+		if (!$.isNumeric($("#phone").val())){
+			invalid++
+			$('#errorTele').show();
+
+		}
+
+		if ($("#pass1").val()!=$("#pass2").val()){
+			invalid++
+			$(".errorPass").show();
+
+		}
+
+		if (invalid === 0 ){
+
+			console.log("completed");
+
+		}
+		
+
+	});
+
